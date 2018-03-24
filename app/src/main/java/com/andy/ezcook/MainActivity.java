91b9220f6d22d9a5672a1aa75031169ab4991289
SujeethJinesh <sujeethjinesh@gmail.com>
2018-03-24 14:34:53 -0700

    Add Firebase

--- app/src/main/java/com/andy/ezcook/MainActivity.java
+++ app/src/main/java/com/andy/ezcook/MainActivity.java
@@ -6,13 +6,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "1";
     private RecyclerView mRecyclerView;
     private RecyclerView.LayoutManager mLayoutManager;
     private RecyclerView.Adapter mAdapter;
+    private FirebaseDatabase database;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +31,29 @@ public class MainActivity extends AppCompatActivity {
 
         Button updateImageButton = findViewById(R.id.updateImageButton);
         RecyclerView mRecyclerView = findViewById(R.id.recyclerView);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference imagesRef = database.getReference("images"); // ref for where we store images
+
+        imagesRef.setValue("Hello World!");
+
+        DatabaseReference recipesRef = database.getReference("recipes");
+
+        recipesRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
 
         mRecyclerView = findViewById(R.id.recyclerView);
 
