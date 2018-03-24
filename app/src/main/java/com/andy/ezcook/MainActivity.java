051677546e5ced43c459199e2dea4b5b046fcee5
SujeethJinesh <sujeethjinesh@gmail.com>
2018-03-24 13:52:09 -0700

    Add Camera Functionality

--- app/src/main/java/com/andy/ezcook/MainActivity.java
+++ app/src/main/java/com/andy/ezcook/MainActivity.java
@@ -1,8 +1,11 @@
 package com.andy.ezcook;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,5 +17,21 @@ public class MainActivity extends AppCompatActivity {
 
         Button updateImageButton = findViewById(R.id.updateImageButton);
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
+
+        updateImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
+            }
+        });
+    }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
     }
 }
